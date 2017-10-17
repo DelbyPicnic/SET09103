14fe25ab38395bdd9de62b35391f6969ef35a7f4
Gordon Swan <gordon@lenovo.localhost>
2017-10-17 02:45:14 +0100

    Added a simple viewer, stll a lot of work todo

--- main.py
+++ main.py
@@ -8,5 +8,9 @@ def home():
 	data = db.getDataQ(30)
 	return render_template("index.html", data=data)
 
+@app.route('/view/<imgID>')
+def view(imgID = None):
+	return render_template("view.html", img_id=imgID)
+
 if __name__ == "__main__": 
 	app.run(host='0.0.0.0', debug=True)
