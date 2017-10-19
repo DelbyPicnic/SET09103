07b3efc9db40d0892992dd56a194508b59b3fe34
Gordon Swan <gordon@Hackintosh.local>
2017-10-19 14:19:06 +0100

    Linked pages together and added search functionality, working on getting the page linking correct

--- main.py
+++ main.py
@@ -1,16 +1,33 @@
-from flask import Flask, render_template
+from flask import Flask, request, render_template
 import data_access as db
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-	data = db.getDataQ(30)
+	sTerm = request.args.get('search', '')
+	
+	if sTerm == '':
+		data = db.getDataQ(30)
+	else:
+		data = db.searchData(sTerm)
+
+	return render_template("index.html", data=data)
+
+@app.route('/search/')
+def search():
+	sTerm = request.args.get('t', '')
+	if sTerm == '':
+		return "Could not be found"
+	else:
+		data = db.searchData(sTerm)
+
 	return render_template("index.html", data=data)
 
 @app.route('/view/<imgID>')
 def view(imgID = None):
-	return render_template("view.html", img_id=imgID)
+	data = db.getRecord(imgID)
+	return render_template("view.html", data=data)
 
 if __name__ == "__main__": 
 	app.run(host='0.0.0.0', debug=True)
